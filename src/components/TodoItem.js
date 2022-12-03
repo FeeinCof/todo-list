b1944742c31f03de4c6c8033d955debc8504d485
FeeinCof <tuan.letrong.dev@gmail.com>
2022-12-04 04:07:49 +0700

    finish TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,1 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './TodoItem.css';
+import classNames from 'classnames';
+class TodoItem extends Component {
+    render() {
+        const { item } = this.props;
+        return (
+            <>
+                <div className ={classNames('TodoItem', {'TodoItem_complete': item.isComplete})}>
+                    <p>{ item.title }</p>
+                </div>
+            </>
+        )
+    }
+}
+
+export default TodoItem;
